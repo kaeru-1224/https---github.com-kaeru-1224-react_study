109e3f120aef96288f400719dd7904208d0d6559
didi <cjfslzpf@naver.com>
2021-11-29 18:51:45 +0900

    react_day1

---
+++ src/Todolist.js
@@ -0,0 +1,25 @@
+import React from "react";
+function Main() {
+  return (
+    <section className="main">
+      <input
+        id="toggle-all"
+        className="toggle-all"
+        type="checkbox"
+        checked=""
+      />
+      <label htmlFor="toggle-all"></label>
+      <ul className="todo-list">
+        <li className="completed">
+          <div className="view">
+            <input className="toggle" type="checkbox" checked={false} />
+            <label>투두 리스트</label>
+            <button className="destroy"></button>
+          </div>
+          <input className="edit" value="투두 리스트" />
+        </li>
+      </ul>
+    </section>
+  );
+}
+export default Main;
